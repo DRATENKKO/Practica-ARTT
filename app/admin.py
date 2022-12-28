460337dbd95225a1f3e571e3a4e7218d78b8c8f5
Dratenkko <sebastian.vargas.bermejo@gmail.com>
2022-12-28 09:25:26 -0300

    vistas

--- app/admin.py
+++ app/admin.py
@@ -10,14 +10,23 @@ class MemoriceAdmin(admin.ModelAdmin):
     list_display = ["usuario", "acierto", "tiempo",
                     "movimientos", "timestamp"]
 
+class Resultado_juegoAdmin(admin.ModelAdmin):
+    list_display = ["id_resultado", "id_usuario", "id_juego",
+                    "resultado_1", "resultado_2", "resultado_3",
+                    "resultado_4", "resultado_5","timestampp"]
+
+class galleryAdmin(admin.ModelAdmin):
+    list_display = ["id", "image", "user","timestamp"]
+
+
 
 admin.site.register(Memorice, MemoriceAdmin)
 admin.site.register(Galeria)
 admin.site.register(Tipo_juego)
 admin.site.register(Juego)
-admin.site.register(Resultado_juego)
+admin.site.register(Resultado_juego,Resultado_juegoAdmin)
 admin.site.register(Usuario)
 admin.site.register(Tipo_usuario)
 admin.site.register(Terapista)
-admin.site.register(gallery)
+admin.site.register(gallery,galleryAdmin)
 
--- app/models.py
+++ app/models.py
@@ -140,8 +140,10 @@ class Galeria(models.Model):
 
 
 class gallery(models.Model):
+    id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to='user-', null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.user)
